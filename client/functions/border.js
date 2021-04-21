155fd1a2b26045593803db451d9edd7a846e5a8e
Jason-Li0827 <lijason0827@gmail.com>
2021-04-21 11:49:01 +0300

    asd
    asd

--- client/functions/border.js
+++ client/functions/border.js
@@ -1,71 +1,71 @@
-function borderCheck(client) {
+function borderCheck(user) {
   obstacles.forEach(function(obs) {
-      //Checks if client is out of the borders
-      if (client.movement.dir === "r") {
-          if (client.pos.x + client.r >= obs.x && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x <= obs.x) { //Checks if client is next to the LEFT border of obs
-              client.pos.x = obs.x - client.r - 1;
+      //Checks if user is out of the borders
+      if (user.movement.dir === "r") {
+          if (user.pos.x + user.r >= obs.x && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x <= obs.x) { //Checks if user is next to the LEFT border of obs
+              user.pos.x = obs.x - user.r - 1;
               rightPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "dr") {
-          if (client.pos.x + client.r >= obs.x && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x <= obs.x) { //Checks if client is next to the LEFT border of obs
-              client.pos.x = obs.x - client.r - 1;
+      } else if (user.movement.dir === "dr") {
+          if (user.pos.x + user.r >= obs.x && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x <= obs.x) { //Checks if user is next to the LEFT border of obs
+              user.pos.x = obs.x - user.r - 1;
               rightPressed = false;
               player.movement.dir = "";
           };
-          if (client.pos.y + client.r >= obs.y && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y <= obs.y) { //Checks if client is next to the UPPER border of obs
-              client.pos.y = obs.y - client.r - 1;
+          if (user.pos.y + user.r >= obs.y && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y <= obs.y) { //Checks if user is next to the UPPER border of obs
+              user.pos.y = obs.y - user.r - 1;
               downPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "d") {
-          if (client.pos.y + client.r >= obs.y && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y <= obs.y) { //Checks if client is next to the UPPER border of obs
-              client.pos.y = obs.y - client.r - 1;
+      } else if (user.movement.dir === "d") {
+          if (user.pos.y + user.r >= obs.y && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y <= obs.y) { //Checks if user is next to the UPPER border of obs
+              user.pos.y = obs.y - user.r - 1;
               downPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "dl") {
-          if (client.pos.y + client.r >= obs.y && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y <= obs.y) { //Checks if client is next to the UPPER border of obs
-              client.pos.y = obs.y - client.r - 1;
+      } else if (user.movement.dir === "dl") {
+          if (user.pos.y + user.r >= obs.y && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y <= obs.y) { //Checks if user is next to the UPPER border of obs
+              user.pos.y = obs.y - user.r - 1;
               downPressed = false;
               player.movement.dir = "";
           };
-          if (client.pos.x - client.r <= obs.x + obs.width && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x >= obs.x + obs.width) { //Checks if client is next to the RIGHT border of obs
-              client.pos.x = obs.x + obs.width + client.r + 1;
+          if (user.pos.x - user.r <= obs.x + obs.width && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x >= obs.x + obs.width) { //Checks if user is next to the RIGHT border of obs
+              user.pos.x = obs.x + obs.width + user.r + 1;
               leftPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "l") {
-          if (client.pos.x - client.r <= obs.x + obs.width && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x >= obs.x + obs.width) { //Checks if client is next to the RIGHT border of obs
-              client.pos.x = obs.x + obs.width + client.r + 1;
+      } else if (user.movement.dir === "l") {
+          if (user.pos.x - user.r <= obs.x + obs.width && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x >= obs.x + obs.width) { //Checks if user is next to the RIGHT border of obs
+              user.pos.x = obs.x + obs.width + user.r + 1;
               leftPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "ul") {
-          if (client.pos.x - client.r <= obs.x + obs.width && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x >= obs.x + obs.width) { //Checks if client is next to the RIGHT border of obs
-              client.pos.x = obs.x + obs.width + client.r + 1;
+      } else if (user.movement.dir === "ul") {
+          if (user.pos.x - user.r <= obs.x + obs.width && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x >= obs.x + obs.width) { //Checks if user is next to the RIGHT border of obs
+              user.pos.x = obs.x + obs.width + user.r + 1;
               leftPressed = false;
               player.movement.dir = "";
           };
-          if (client.pos.y - client.r <= obs.y + obs.height && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y >= obs.y + obs.height) { //Checks if client is next to the LOWER border of obs
-              client.pos.y = obs.y + obs.height + client.r + 1;
+          if (user.pos.y - user.r <= obs.y + obs.height && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y >= obs.y + obs.height) { //Checks if user is next to the LOWER border of obs
+              user.pos.y = obs.y + obs.height + user.r + 1;
               upPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "u") {
-          if (client.pos.y - client.r <= obs.y + obs.height && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y >= obs.y + obs.height) { //Checks if client is next to the LOWER border of obs
-              client.pos.y = obs.y + obs.height + client.r + 1;
+      } else if (user.movement.dir === "u") {
+          if (user.pos.y - user.r <= obs.y + obs.height && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y >= obs.y + obs.height) { //Checks if user is next to the LOWER border of obs
+              user.pos.y = obs.y + obs.height + user.r + 1;
               upPressed = false;
               player.movement.dir = "";
           };
-      } else if (client.movement.dir === "ur") {
-          if (client.pos.y - client.r <= obs.y + obs.height && client.pos.x + client.r >= obs.x && client.pos.x - client.r <= obs.x + obs.width && client.pos.y >= obs.y + obs.height) { //Checks if client is next to the LOWER border of obs
-              client.pos.y = obs.y + obs.height + client.r + 1;
+      } else if (user.movement.dir === "ur") {
+          if (user.pos.y - user.r <= obs.y + obs.height && user.pos.x + user.r >= obs.x && user.pos.x - user.r <= obs.x + obs.width && user.pos.y >= obs.y + obs.height) { //Checks if user is next to the LOWER border of obs
+              user.pos.y = obs.y + obs.height + user.r + 1;
               upPressed = false;
               player.movement.dir = "";
           };
-          if (client.pos.x + client.r >= obs.x && client.pos.y + client.r >= obs.y && client.pos.y - client.r <= obs.y + obs.height && client.pos.x <= obs.x) { //Checks if client is next to the LEFT border of obs
-              client.pos.x = obs.x - client.r - 1;
+          if (user.pos.x + user.r >= obs.x && user.pos.y + user.r >= obs.y && user.pos.y - user.r <= obs.y + obs.height && user.pos.x <= obs.x) { //Checks if user is next to the LEFT border of obs
+              user.pos.x = obs.x - user.r - 1;
               rightPressed = false;
               player.movement.dir = "";
           };
