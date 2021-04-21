15e3ea13fbc549f04865f2a251bab6c960bca5d0
Jason-Li0827 <lijason0827@gmail.com>
2021-04-21 11:44:42 +0300

    random stuff

--- server/serverFunctions.js
+++ server/serverFunctions.js
@@ -8,4 +8,19 @@ function getUserById(users, id) {
   return users.find(user => user.id === id);
 }
 
-module.exports = { getRoomById, getUserById };
+class obstacle {
+  constructor(x, y,imgName) {
+      this.x = x;
+      this.y = y;
+      if (imgName == "roofBlue"){
+        this.width = 150;
+        this.height = 150;
+      }
+  }
+
+  draw() {
+      ctx.drawImage(this.img, this.x, this.y);
+  }
+}
+
+module.exports = { getRoomById, getUserById ,obstacle};
