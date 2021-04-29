8083e1e3f0bf3df74ff48eafe4da2037bfc59c10
Robiis <pakarklis.roberts@gmail.com>
2021-04-29 22:26:37 +0300

    loader test 4

--- client/functions/imageWaiting.js
+++ client/functions/imageWaiting.js
@@ -6,7 +6,7 @@
 // Gaidīšanas laikā lapas apakšā (pēc visiem elementiem) uzrādās "Attēlu ielāde ...", kas pazūd brīdī, kad visi attēli ir ielādēti.
 // Autors:: 2018-2019 Ojārs Krūmiņš
 // Versija 1.3
-// samainitas dazas lietas
+// Noņemts "Attēlu ielāde" teksts
 function gaidaAtteluIeladi() {
   var images = [].slice.call(arguments); var num = arguments.length;
   var interval = setInterval(function(){ var n = 1; for (var i = 1; i < images.length; i++){ if( images[i].complete ) { n++; } }
